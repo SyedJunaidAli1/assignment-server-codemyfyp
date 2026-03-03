5e16b0297d80b9e3b72bc5e9debba81317a936e3
SyedJunaidAli1 <syedjunaidali790@gmail.com>
2026-03-03 16:06:38 +0530

    Auth Routes and add in app.js

--- app.js
+++ app.js
@@ -8,6 +8,10 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+import authRoutes from "./routes/authRoutes.js";
+
+app.use("/api/auth", authRoutes);
+
 // Routes
 app.get("/", (req, res) => {
   res.json({ message: "ShaadiBio API running 🚀" });
