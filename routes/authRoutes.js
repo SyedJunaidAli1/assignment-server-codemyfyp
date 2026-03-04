95524621ceb5d9518c236299a5aceb55e5310dba
SyedJunaidAli1 <syedjunaidali790@gmail.com>
2026-03-04 14:25:19 +0530

    authmiddleware added and functional

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,7 +1,11 @@
 import express from "express";
 const router = express.Router();
 import { register, login } from "../controllers/authController.js";
+import { protect } from "../middlewares/authMiddleware.js";
 
+router.get("/me", protect, (req, res) => {
+  res.json(req.user);
+});
 router.post("/register", register);
 router.post("/login", login);
 
